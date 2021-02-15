d6ce9509443ee1e967ec0bd033899d91d30c28a0
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-02-15 17:32:34 +0100

    added hero section

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,15 @@
 import '../styles/css/app.css';
 //importing components
 import Header from './Header';
+import Hero from './Hero';
 //app component
 const App: React.FC = () => {
   return (
     <div className="app__container">
-      <div className="wrapper"></div>
       <Header />
+      <div className="wrapper">
+        <Hero />
+      </div>
     </div>
   );
 };

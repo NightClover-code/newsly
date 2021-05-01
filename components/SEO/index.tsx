1b68833dcbe45a4bd2815bc8db60378599ebde51
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-05-01 02:18:09 +0200

    adding a favicon and cleanup

---
+++ components/SEO/index.tsx
@@ -0,0 +1,23 @@
+//importing utils
+import Head from 'next/head';
+//props interface
+interface SE0Props {
+  title: string;
+  description: string;
+  keywords?: string;
+}
+//seo
+const SEO: React.FC<SE0Props> = ({ title, description, keywords }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <link rel="shortcut icon" type="images/ico" href="/images/favicon.ico" />
+      <meta name="description" content={description} />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      {keywords && <meta name="keywords" content={keywords} />}
+      <meta name="author" content="Achraf Dev" />
+    </Head>
+  );
+};
+
+export default SEO;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,14 @@ import Articles from '../components/Articles';
 import Categories from '../components/Categories';
 import Hero from '../components/Hero';
 import Newsletter from '../components/Newsletter';
+import SEO from '../components/SEO';
+//importing utils
+import { seoConfigHomepage } from '../utils';
 //homepage
 const Homepage: React.FC = () => {
   return (
     <>
+      <SEO {...seoConfigHomepage} />
       <div className="wrapper">
         <Hero />
         <Categories />

6a5ad4580fdf8aabd1d8c7ec7115b5ffe0c18961
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-07-06 14:55:18 +0100

    adding a responsive carousel

--- components/Categories/index.tsx
+++ components/Categories/index.tsx
@@ -16,25 +16,44 @@ const Categories: React.FC<CategoriesProps> = ({ images: { iconSearch } }) => {
   const gridRef = useRef<HTMLDivElement | null>(null);
 
   const [counter, setCounter] = useState(0);
+  const [responsiveNum, setResponsiveNum] = useState(1);
 
   const gap = 50;
   const distance = 202 + gap;
 
   const onArrowRightClickHandler = () => {
-    setCounter(counter + 1);
+    if (counter < responsiveNum) setCounter(counter + 1);
   };
 
   const onArrowLeftClickHandler = () => {
-    setCounter(counter - 1);
+    if (counter >= responsiveNum) setCounter(counter - 1);
   };
 
-  // useEffect(() => {
-  //   if (gridRef.current) {
-  //     gridRef.current.style.transform =
-  //       'translateX(' + -distance * counter + 'px)';
-  //     gridRef.current.style.transition = 'all 0.5s ease-in-out';
-  //   }
-  // }, [counter]);
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 970) {
+        setResponsiveNum(1);
+      } else if (670 < window.innerWidth && window.innerWidth < 970) {
+        setResponsiveNum(2);
+      } else if (400 < window.innerWidth && window.innerWidth < 670) {
+        setResponsiveNum(3);
+      } else if (window.innerWidth < 400) {
+        setResponsiveNum(4);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (gridRef.current) {
+      gridRef.current.style.transform =
+        'translateX(' +
+        ((-gridRef.current.scrollWidth + gridRef.current.clientWidth) /
+          responsiveNum) *
+          counter +
+        'px)';
+      gridRef.current.style.transition = 'all 0.5s ease-in-out';
+    }
+  }, [counter]);
 
   return (
     <section className="categories__section">

4cb4f2ad0a45860c477c56cf3e5e1de4014cccd8
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-07-09 02:45:54 +0100

    logo design fix & articles gap reduce

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,6 @@
+//importing hooks & context
+import { useContext } from 'react';
+import { WindowContext } from '../../context';
 //importing utils & components
 import Link from 'next/link';
 import Image from 'next/image';
@@ -12,34 +15,38 @@ interface HeaderProps {
 }
 
 //header
-const Header: React.FC<HeaderProps> = ({ images }) => (
-  <header>
-    <div className="container">
-      <Link href="/" passHref>
-        <div className="logo__container">
-          <Image
-            src={images ? images.logo : cloudinaryNotFoundURLs.logo}
-            alt="logo"
-            width="270"
-            height="70"
-          />
-        </div>
-      </Link>
-      <nav>
-        <div className="nav__list">
-          <li className="list__item">Pricing</li>
-          <li className="list__item">Contact</li>
-          <li className="list__item">Services</li>
-          <li className="list__item active">Learn</li>
-        </div>
-        <div className="auth__list">
-          <div className="list__item">Sign In</div>
-          <div className="button primary__button">Get Started</div>
-        </div>
-      </nav>
-      <MenuIcon />
-    </div>
-  </header>
-);
+const Header: React.FC<HeaderProps> = ({ images }) => {
+  const { width } = useContext(WindowContext);
+
+  return (
+    <header>
+      <div className="container">
+        <Link href="/" passHref>
+          <div className="logo__container">
+            <Image
+              src={images ? images.logo : cloudinaryNotFoundURLs.logo}
+              alt="logo"
+              width={width! < 1100 ? '230' : '270'}
+              height="70"
+            />
+          </div>
+        </Link>
+        <nav>
+          <div className="nav__list">
+            <li className="list__item">Pricing</li>
+            <li className="list__item">Contact</li>
+            <li className="list__item">Services</li>
+            <li className="list__item active">Learn</li>
+          </div>
+          <div className="auth__list">
+            <div className="list__item">Sign In</div>
+            <div className="button primary__button">Get Started</div>
+          </div>
+        </nav>
+        <MenuIcon />
+      </div>
+    </header>
+  );
+};
 
 export default Header;

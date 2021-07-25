c84852cf32e6abc4df13b490da57575f3b9b8e8e
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-07-25 15:29:38 +0100

    adding responsive container to height

--- components/Articles/ArticlesItem/index.tsx
+++ components/Articles/ArticlesItem/index.tsx
@@ -1,5 +1,7 @@
 //importing utils
 import Image from 'next/image';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { WindowContext } from '../../../context';
 //importing types
 import { Article } from '../../../interfaces';
 import { ReadMoreArrowRight } from '../../Icons';
@@ -13,9 +15,21 @@ interface ArticlesItemProps {
 const ArticlesItem: React.FC<ArticlesItemProps> = ({ article }) => {
   const { description, title, urlToImage, author } = article;
 
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const [containerHeight, setContainerHeight] = useState(
+    containerRef.current?.clientHeight
+  );
+
+  const { width } = useContext(WindowContext);
+
+  useEffect(() => {
+    setContainerHeight(containerRef.current?.clientHeight! + 60);
+  }, [width]);
+
   return (
-    <div className="articles__item">
-      <div className="container">
+    <div className="articles__item" style={{ maxHeight: containerHeight }}>
+      <div className="container" ref={containerRef}>
         <div className="image__container">
           {urlToImage && (
             <Image
--- components/Articles/index.tsx
+++ components/Articles/index.tsx
@@ -2,11 +2,7 @@
 import { Article } from '../../interfaces';
 import { v4 as uuidv4 } from 'uuid';
 //importing hooks
-import { Fragment, useContext } from 'react';
 import ArticlesItem from './ArticlesItem';
-import { isIndexValid, isLastIndex, isResponsiveIndexValid } from '../../utils';
-//importing context
-import { WindowContext } from '../../context';
 
 //props interface
 interface ArticlesProps {
@@ -15,38 +11,13 @@ interface ArticlesProps {
 
 //articles grid
 const Articles: React.FC<ArticlesProps> = ({ articles }) => {
-  const { width } = useContext(WindowContext);
-
   return (
     <section className="articles__section">
       <h1>Articles</h1>
       <div className="articles__grid">
-        {articles.map((article, index) => {
-          if (isIndexValid(articles, index) && width! > 1300)
-            return (
-              <Fragment key={uuidv4()}>
-                <ArticlesItem article={article} />
-                <div className="horizental__line"></div>
-              </Fragment>
-            );
-          else if (isResponsiveIndexValid(articles, index) && width! <= 1300)
-            return (
-              <Fragment key={uuidv4()}>
-                <ArticlesItem article={article} />
-                <div className="horizental__line"></div>
-              </Fragment>
-            );
-          else
-            return isLastIndex(articles, index) ? (
-              <ArticlesItem article={article} />
-            ) : (
-              <Fragment key={uuidv4()}>
-                <ArticlesItem article={article} />
-                <div className="horizental__line"></div>
-                <div className="vertical__line"></div>
-              </Fragment>
-            );
-        })}
+        {articles.map(article => (
+          <ArticlesItem key={uuidv4()} article={article} />
+        ))}
       </div>
       <div className="button secondary__button">Show More</div>
     </section>
